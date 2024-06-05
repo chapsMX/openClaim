7466b0f38fe9b260758a62dc97a48fe60feaa246
chapsMX <72465656+chapsMX@users.noreply.github.com>
2024-06-05 15:25:24 -0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,34 @@
 import Head from "next/head";
 
-const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
-const title = "Transaction Farcaster Frame on OP";
-const frameImgUrl = `${HOST_URL}/NFTImage.png`;
+const title ="Base Frens Mint Frame";
+const HOST_URL = process.env.PUBLIC_URL
+const imagenFrame = `${HOST_URL}/placeHolder.png`;
 
 export default function Home() {
   return (
     <>
       <Head>
-        <meta property="og:title" content={title} />
+        <meta property="og:title" content={title}/>
         <meta property="fc:frame" content="vNext"/>
-        <meta name="fc:frame:image" content={frameImgUrl}/>
+        <meta name="fc:frame:image" content={imagenFrame}/>
         <meta name="fc:frame:image:aspect_ratio" content="1:1"/>
-        <meta property="fc:frame:button:1" content="Mint NFT"/>
+        {/* mint 1 basefren */}
+        <meta property="fc:frame:button:1" content="Mint 1 BaseFren"/>
         <meta property="fc:frame:button:1:action" content="tx"/>
         <meta 
-          property="fc:frame:button:1:target"
-          content={`${HOST_URL}/api/frame/op/get-tx-frame`}
-        />
+           property="fc:frame:button:1:target"
+           content={`${HOST_URL}/api/frame/base/tx-1`}
+           />
+        {/* mint 3 basefren */}
+        <meta property="fc:frame:button:2" content="Mint 3 BaseFRens"/>
+        <meta property="fc:frame:button:2:action" content="tx"/>
+        <meta 
+           property="fc:frame:button:2:target"
+           content={`${HOST_URL}/api/frame/base/tx-3`}
+           />   
       </Head>
-      <h1>Transaction Frame</h1>
+       <h1>Base Frens Mint Frame</h1>
     </>
   );
 }
+
